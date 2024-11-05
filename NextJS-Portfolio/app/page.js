c92eb4ca5072e4bcbc55dd87fd8db4c88a3b21b9
mssbabou <46846045+mssbabou@users.noreply.github.com>
2024-11-05 13:45:10 +0100

    Update page.js

--- NextJS-Portfolio/app/page.js
+++ NextJS-Portfolio/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
     const observerOptions = {
       root: null,
       rootMargin: "0px",
-      threshold: 0.25,
+      threshold: 0.4,
     };
 
     const observerCallback = (entries) => {
