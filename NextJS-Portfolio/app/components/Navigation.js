2f6ba9cef2856e2c903c1240d59d41014bb0813b
Markus Steen Sørensen <mkssr@vestas.com>
2024-11-13 10:02:03 +0100

    Polish Contacts Menu

--- NextJS-Portfolio/app/components/Navigation.js
+++ NextJS-Portfolio/app/components/Navigation.js
@@ -5,11 +5,11 @@ import { sections } from "../data/sections";
 export default function Navigation({ activeSection, scrollToSection, className }) {
   const [showContacts, setShowContacts] = useState(false);
 
+  const handleMouseEnterContacts = () => setShowContacts(true);
+  const handleMouseLeaveContacts = () => setShowContacts(false);
+
   return (
-    <div
-      className={`${className} relative`}
-      onMouseLeave={() => setShowContacts(false)}
-    >
+    <div className={`${className} relative`}>
       {/* Main Navigation */}
       <div className="fixed top-6 left-6 z-10 text-2xl text-hjalmarBlue">
         <div className="flex">
@@ -18,7 +18,8 @@ export default function Navigation({ activeSection, scrollToSection, className }
               <button
                 key={section.id}
                 onClick={() => scrollToSection(section.id)}
-                className={`flex items-center px-3 py-1 bg-transparent rounded ${
+                onMouseEnter={() => setShowContacts(false)} // Close contacts menu when hovering other buttons
+                className={`flex items-center px-3 py-1 bg-transparent rounded hover:font-bold ${
                   activeSection === section.id ? "font-bold" : ""
                 }`}
               >
@@ -34,7 +35,8 @@ export default function Navigation({ activeSection, scrollToSection, className }
             ))}
             {/* Contacts Button */}
             <button
-              onMouseEnter={() => setShowContacts(true)}
+              onMouseEnter={handleMouseEnterContacts}
+              onMouseLeave={handleMouseLeaveContacts}
               className={`flex items-center px-3 py-1 bg-transparent rounded ${
                 showContacts ? "font-bold" : ""
               }`}
@@ -43,25 +45,31 @@ export default function Navigation({ activeSection, scrollToSection, className }
               Contacts
             </button>
           </nav>
+
           {/* Contacts Menu - Positioned next to the main navigation */}
           {showContacts && (
-            <div className="ml-3 p-4 backdrop-blur-lg bg-white/20 border border-white/30 shadow-lg rounded-xl text-hjalmarBlue transition-opacity duration-200">
-              <div className="flex flex-col justify-between h-full text-xl">
-                <div className="flex items-center">
-                  <Call fontSize="large" className="mr-3" />
-                  <a href="tel:+4542583158">+45 42 58 31 58</a>
-                </div>
-                <div className="flex items-center">
-                  <Mail fontSize="large" className="mr-3" />
-                  <a href="mailto:hjalmargraphics@gmail.com">hjalmargraphics@gmail.com</a>
-                </div>
-                <div className="flex items-center">
-                  <img
-                    src="/linkedin.svg"
-                    style={{ width: "36px" }}
-                    className="mr-3"
-                  />
-                  <a href="https://www.linkedin.com/in/hjalmar-kjeldsen-b4264826a" target="_blank" rel="noopener">Hjalmar Kjeldsen</a>
+            <div className="pl-3"
+              onMouseEnter={handleMouseEnterContacts}
+              onMouseLeave={handleMouseLeaveContacts}>
+              <div
+                className="p-4 h-full backdrop-blur-lg bg-white/20 border border-white/30 shadow-lg rounded-xl text-hjalmarBlue transition-opacity duration-200">
+                <div className="flex flex-col justify-between h-full text-xl">
+                  <div className="flex items-center">
+                    <Call fontSize="large" className="mr-3" />
+                    <a href="tel:+4542583158">+45 42 58 31 58</a>
+                  </div>
+                  <div className="flex items-center">
+                    <Mail fontSize="large" className="mr-3" />
+                    <a href="mailto:hjalmargraphics@gmail.com">hjalmargraphics@gmail.com</a>
+                  </div>
+                  <div className="flex items-center">
+                    <img
+                      src="/linkedin.svg"
+                      style={{ width: "36px" }}
+                      className="mr-3"
+                    />
+                    <a href="https://www.linkedin.com/in/hjalmar-kjeldsen-b4264826a" target="_blank" rel="noopener">Hjalmar Kjeldsen</a>
+                  </div>
                 </div>
               </div>
             </div>
